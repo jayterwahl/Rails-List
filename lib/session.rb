f5be6558cc4cb83aa4a11695e503d3038e935652
App Academy Student <student@appacademy.io>
2016-02-02 17:55:31 -0800

    Introduce Thin web server compatibility

---
+++ lib/session.rb
@@ -0,0 +1,28 @@
+require 'json'
+
+class Session
+  # find the cookie for this app
+  # deserialize the cookie into a hash
+  def initialize(req)
+    if req.cookies["_rails_lite_app"]
+      @parsed = JSON.parse(req.cookies["_rails_lite_app"])
+    else
+      @parsed = {}
+    end
+  end
+
+  def [](key)
+    @parsed[key]
+  end
+
+  def []=(key, val)
+    @parsed[key] = val
+  end
+
+  # serialize the hash into json and save in a cookie
+  # add to the responses cookies
+  def store_session(res)
+    cookie = { path: '/', value: @parsed.to_json }
+    res.set_cookie("_rails_lite_app", cookie)
+  end
+end
